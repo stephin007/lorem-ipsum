bdb548cbcb807f20bc6ce671518ad00372ad5e60
stephin007 <stephinreji123@gmail.com>
2021-05-01 12:23:16 +0530

    Added the form:pencil:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {useState} from "react"
+
+const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log("hello world")
+}
+
 function App() {
+    const [count, setCount] = useState(0)
   return (
     <section className="section-center">
         <h3>tired of lorem ipsum</h3>
+        <form className="lorem-form" onSubmit={handleSubmit}>
+            <label htmlFor="amount">
+                paragraphs:
+            </label>
+            <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)}/>
+            <button className="btn">
+                generate
+            </button>
+        </form>
     </section>
   );
 }
