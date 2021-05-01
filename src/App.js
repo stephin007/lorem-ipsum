59524fdd38e5c9561abe3c94b11579da7050eadc
stephin007 <stephinreji123@gmail.com>
2021-05-01 19:21:29 +0530

    Introduced version type for lorem-ipsum:tada:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {useState} from "react"
 
-const url = `https://hipsum.co/api/?type=hipster-latin&paras=50`
 function App() {
-
     const [count, setCount] = useState(0)
     const [text, setText] = useState([])
-
+    const [type, setType] = useState("hipster-latin")
     const handleSubmit= async (e)=>{
+
+        const url = `https://hipsum.co/api/?type=${type}&paras=50`
         e.preventDefault()
         //if you console.log the count you will see that count is a string not a number, its not necessary but its a
         // good practice to convert string to number if you are working with number
@@ -14,6 +14,7 @@ function App() {
 
         const response = await fetch(url)
         const newTexts = await response.json()
+        console.log(newTexts)
 
         //condition if count becomes zero or goes below zero, it will show atleast one paragraph
         if(count <= 0){
@@ -30,10 +31,15 @@ function App() {
         <section className="section-center">
             <h3>tired of lorem ipsum</h3>
             <form className="lorem-form" onSubmit={handleSubmit}>
-                <label htmlFor="amount">
+                <label htmlFor="amount" className="lorem-type">
                     paragraphs:
                 </label>
                 <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)}/>
+                <label htmlFor="type" className="lorem-type">Choose a version:</label>
+                <select name="types" id="types" onChange={(e)=> setType(e.target.value)}>
+                    <option value="hipster-latin">Hipster Speak Only </option>
+                    <option value="hipster-centric">Hipster Speak with Latin</option>
+                </select>
                 <button className="btn">
                     generate
                 </button>
