1ea2874ff03e0005f2132a8139d885156f9c61eb
stephin007 <stephinreji123@gmail.com>
2021-05-01 12:35:44 +0530

    Iterating over the static paragraphs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {useState} from "react"
-
-const handleSubmit=(e)=>{
-    e.preventDefault()
-    console.log("hello world")
-}
+import data from './data';
 
 function App() {
     const [count, setCount] = useState(0)
+    const [text, setText] = useState([])
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setText(data)
+    }
   return (
     <section className="section-center">
         <h3>tired of lorem ipsum</h3>
@@ -19,6 +21,13 @@ function App() {
                 generate
             </button>
         </form>
+        <article className="lorem-text">
+            {text.map((item, index)=> {
+                return (
+                    <p key={index}>{item}</p>
+                )
+            })}
+        </article>
     </section>
   );
 }
