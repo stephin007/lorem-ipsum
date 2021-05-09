0a55d027735ffc278287985d8ce36014b2ced29a
Restyled.io <commits@restyled.io>
2021-05-09 03:09:19 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
 import "./DarkStyles/DarkStyle.css";
+
 import ToggleOffIcon from "@material-ui/icons/ToggleOff";
 import ToggleOnIcon from "@material-ui/icons/ToggleOn";
+import React, {useEffect, useState} from "react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +19,8 @@ function App() {
     setShowLoading(true);
     const url = `https://hipsum.co/api/?type=${type}&paras=50`;
     e.preventDefault();
-    //if you console.log the count you will see that count is a string not a number, its not necessary but its a
+    // if you console.log the count you will see that count is a string not a
+    // number, its not necessary but its a
     // good practice to convert string to number if you are working with number
     let amount = parseInt(count);
 
@@ -26,14 +28,16 @@ function App() {
     const newTexts = await response.json();
     console.log(newTexts);
 
-    //condition if count becomes zero or goes below zero, it will show atleast one paragraph
+    // condition if count becomes zero or goes below zero, it will show atleast
+    // one paragraph
     if (count <= 0) {
       amount = 1;
     }
-    //as soon as the text data is ready, turn off the loading GIF
+    // as soon as the text data is ready, turn off the loading GIF
     setText(newTexts.slice(0, amount), setShowLoading(false));
 
-    //condition if count goes above the total length of the data, it will show all the paragraphs inside your array
+    // condition if count goes above the total length of the data, it will show
+    // all the paragraphs inside your array
     if (count > text.length) {
       amount = text.length;
     }
@@ -43,7 +47,7 @@ function App() {
     localStorage.setItem("theme", JSON.stringify(dark));
     const data = localStorage.getItem("theme");
     setTheme(data);
-  }, [dark]);
+  }, [ dark ]);
 
   return (
     <React.Fragment>
@@ -51,14 +55,14 @@ function App() {
         <div className="app__dark">
           <section className="section-center">
             <ToggleOffIcon
-              onClick={() => {
-                setDark(false);
-              }}
-              fontSize="large"
-              style={{ color: "white" }}
-            />
-            <h4>Toggle Dark mode</h4>
-            <h3>tired of lorem ipsum</h3>
+  onClick = { () => { setDark(false); } } fontSize = "large"
+  style =
+  {
+    { color: "white" }
+  } />
+            <h4>Toggle Dark mode</h4 >
+      <h3>tired of lorem ipsum<
+          /h3>
             <form className="lorem-form" onSubmit={handleSubmit}>
               <div className="cont">
                 <span className="grid-1">
@@ -67,14 +71,15 @@ function App() {
                     className="responsive-text lorem-type"
                   >
                     paragraphs:
-                  </label>
-                  <input
-                    className="responsive-text"
-                    type="number"
-                    name="amount"
-                    id="amount"
+                  </label><
+      input
+  className = "responsive-text"
+  type = "number"
+  name = "amount"
+  id = "amount"
                     value={count}
-                    onChange={(e) => setCount(e.target.value)}
+                    onChange={
+    (e) => setCount(e.target.value)}
                   />
                 </span>
                 <span className="grid-2">
@@ -96,8 +101,10 @@ function App() {
                 <button className="grid-3 btn">generate</button>
               </div>
             </form>
-            {showLoading ? (
-              <div className="loader-gif"></div>
+            {
+    showLoading
+        ? (<div className = "loader-gif">
+           </div>
             ) : (
               <article className="lorem-text">
                 {text.map((item, index) => {
@@ -124,11 +131,11 @@ function App() {
               <div className="cont">
                 <span className="grid-1">
                   <label
-                    htmlFor="amount"
-                    className="responsive-text lorem-type"
-                  >
-                    paragraphs:
-                  </label>
+                    htmlFor = "amount"
+                    className =
+                        "responsive-text lorem-type" >
+                        paragraphs: <
+                            /label>
                   <input
                     className="responsive-text"
                     type="number"
@@ -137,15 +144,15 @@ function App() {
                     value={count}
                     onChange={(e) => setCount(e.target.value)}
                   />
-                </span>
+                        </span>
                 <span className="grid-2">
                   <label htmlFor="type" className="responsive-text lorem-type">
                     Choose a version:
-                  </label>
-                  <select
-                    className="responsive-text"
-                    name="types"
-                    id="types"
+                  </label><
+                        select
+                    className = "responsive-text"
+                    name = "types"
+                    id = "types"
                     onChange={(e) => setType(e.target.value)}
                   >
                     <option value="hipster-latin">Hipster Speak Only </option>
